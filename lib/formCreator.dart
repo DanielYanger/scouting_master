5977012975be2fe41bdce70f010a54fcf0e7ffeb
Daniel Yang <danielyang2002@gmail.com>
2020-06-15 17:13:32 -0500

    Code Clean Up

--- lib/formCreator.dart
+++ lib/formCreator.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:scoutingmaster/scouting_icons_icons.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:scoutingmaster/scouting_icons_icons.dart';
 
 import 'booleanAdder.dart';
 import 'checkboxAdder.dart';
